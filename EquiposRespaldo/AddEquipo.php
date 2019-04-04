e5b178a2437765a7d480e04fca6cd6541985ee30
albertomendozaangeles <47876755+albertomendozaangeles@users.noreply.github.com>
2019-04-04 12:23:43 -0600

    validacion
    validacion aes telefono contaxto

--- EquiposRespaldo/AddEquipo.php
+++ EquiposRespaldo/AddEquipo.php
@@ -6,11 +6,11 @@
     </div>
     <div class="col-md-6">
       Nombre contacto
-      <input type="text" class="form-control" id="Equipo_NombreContacto">
+      <input type="text" class="form-control" id="Equipo_NombreContacto required onkeypress="return soloLetras(event);" onKeyUp="this.value = this.value.toUpperCase();">
     </div>
     <div class="col-md-6">
       Numero contacto
-      <input type="text" class="form-control" id="Equipo_NumeroContacto">
+      <input type="text" class="form-control" id="Equipo_NumeroContacto" required onKeyPress="return SoloNumeros(event);">
     </div>
     <div class="col-md-12">
       Disponible
@@ -149,4 +149,39 @@
           $('#Equipo_MunicipioEquipo').html(response);
       });
   }
+
+  function SoloNumeros(evt){
+ if(window.event){//asignamos el valor de la tecla a keynum
+  keynum = evt.keyCode; //IE
+ }
+ else{
+  keynum = evt.which; //FF
+ } 
+ //comprobamos si se encuentra en el rango numérico y que teclas no recibirá.
+ if((keynum > 47 && keynum < 58) || keynum == 8 || keynum == 13 || keynum == 6 ){
+  return true;
+ }
+ else{
+  return false;
+ }
+}
+function soloLetras(e) {
+    key = e.keyCode || e.which;
+    tecla = String.fromCharCode(key).toString();
+    letras = " áéíóúabcdefghijklmnñopqrstuvwxyzÁÉÍÓÚABCDEFGHIJKLMNÑOPQRSTUVWXYZ";//Se define todo el abecedario que se quiere que se muestre.
+    especiales = [8, 37, 39, 46, 6]; //Es la validación del KeyCodes, que teclas recibe el campo de texto.
+
+    tecla_especial = false
+    for(var i in especiales) {
+        if(key == especiales[i]) {
+            tecla_especial = true;
+            break;
+        }
+    }
+
+    if(letras.indexOf(tecla) == -1 && !tecla_especial){
+alert('Tecla no aceptada');
+        return false;
+      }
+}
 </script>
