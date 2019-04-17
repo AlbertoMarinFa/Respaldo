449bf57ab9243971f62395c82fc6abc51ad4c67b
albertomendozaangeles <47876755+albertomendozaangeles@users.noreply.github.com>
2019-04-17 10:12:13 -0500

    update
    quite validacion

--- EquiposRespaldo/AddEquipo.php
+++ EquiposRespaldo/AddEquipo.php
@@ -3,14 +3,16 @@
     <div class="col-md-12">
       No. serie
       <input type="text"class="form-control" id="Equipo_NoSerieEquipo">
+      
+      
     </div>
     <div class="col-md-6">
       Nombre contacto
-      <input type="text" class="form-control" id="Equipo_NombreContacto required onkeypress="return soloLetras(event);" onKeyUp="this.value = this.value.toUpperCase();">
+      <input type="text"class="form-control" id="Equipo_NombreContacto">
     </div>
     <div class="col-md-6">
       Numero contacto
-      <input type="text" class="form-control" id="Equipo_NumeroContacto" required onKeyPress="return SoloNumeros(event);">
+      <input type="text"class="form-control" id="Equipo_NumeroContacto">
     </div>
     <div class="col-md-12">
       Disponible
@@ -150,38 +152,5 @@
       });
   }
 
-  function SoloNumeros(evt){
- if(window.event){//asignamos el valor de la tecla a keynum
-  keynum = evt.keyCode; //IE
- }
- else{
-  keynum = evt.which; //FF
- } 
- //comprobamos si se encuentra en el rango numérico y que teclas no recibirá.
- if((keynum > 47 && keynum < 58) || keynum == 8 || keynum == 13 || keynum == 6 ){
-  return true;
- }
- else{
-  return false;
- }
-}
-function soloLetras(e) {
-    key = e.keyCode || e.which;
-    tecla = String.fromCharCode(key).toString();
-    letras = " áéíóúabcdefghijklmnñopqrstuvwxyzÁÉÍÓÚABCDEFGHIJKLMNÑOPQRSTUVWXYZ";//Se define todo el abecedario que se quiere que se muestre.
-    especiales = [8, 37, 39, 46, 6]; //Es la validación del KeyCodes, que teclas recibe el campo de texto.
 
-    tecla_especial = false
-    for(var i in especiales) {
-        if(key == especiales[i]) {
-            tecla_especial = true;
-            break;
-        }
-    }
-
-    if(letras.indexOf(tecla) == -1 && !tecla_especial){
-alert('Tecla no aceptada');
-        return false;
-      }
-}
 </script>
